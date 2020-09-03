0bcd64e903e44aee417505bf2eba0d3bde8f9fe2
horizon <liudxp@live.cn>
2020-09-03 10:00:11 +0800

    update

---
+++ test_cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <opencv4/opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
+int main(int argc, char const *argv[]) {
+  for (int i = 0; i < 10; i++) {
+    cout << i * 2 << endl;
+  }
+
+  cout << "hello fuck" << endl;
+  return 0;
+}
